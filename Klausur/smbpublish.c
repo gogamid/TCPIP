03b41802ea8a2e8d4bff4d921303e96260ac7f79
Imron Gamidli <imron.gamidli@lt.hs-fulda.de>
2021-07-09 01:59:28 +0200

    hostname is implemented

--- Klausur/smbpublish.c
+++ Klausur/smbpublish.c
@@ -20,26 +20,6 @@ date
 thrmostat
 */
 
-/*
-UDP read write
-Der Subscriber wird per Kommandozeile mit dem ihn interessierenden topic
-gestartet und soll ebenfalls in einer Dauerschleife auf Nachrichten vom
-Broker warten und diese dann ausgeben
-
-Der Aufruf des Subscribers: smbsubscribe broker topic
-wobei broker der Hostname oder die IP-Adresse des Brokers ist
-und topic das Thema / den Betreff benennt
-
-examples:
-smbsubscribe localhost datum
-smbsubscribe 192.168.1.13 "#"
-
-if topic is # then werden alle Nachrichten vom Broker an den
-Subscriber weitergeleitet
-
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -48,70 +28,80 @@ Subscriber weitergeleitet
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <netdb.h>
 
 // Port
 const int srv_port = 8080;
 
 // Formatstring der Nachricht
-#define SERVER_MESSAGE  "Hi, hier Client %d"
+#define SERVER_MESSAGE "Hi, hier Client %d"
 
 // main
 int main(int argc, char **argv)
 {
-  char *server_ip;                      // Server IP
-  int sock_fd;                          // Socket
-  struct sockaddr_in server_addr;       // Serveradresse
-  socklen_t server_size;                // Adresslaenge
-  char buffer[512];                     // Schreib-/Lesepuffer
-  int nbytes, length;
+    int sock_fd;                    // Socket
+    struct sockaddr_in server_addr; // Serveradresse
+    socklen_t server_size;          // Adresslaenge
+    char buffer[512];               // Schreib-/Lesepuffer
+    int nbytes, length;
 
-  // Kommandozeile bearbeiten
-  if(argc < 2) {
-    fprintf(stderr, "Aufruf: %s Server-IP-Adresse\n", argv[0]);
-    return 1;
-  }
-  // 1. Parameter -> IP-Adresse des Server
-  server_ip = argv[1];
-  
-  // Socket anlegen und oeffnen
-  // Familie: Internet, Typ: UDP-Socket
-  sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
-  fprintf(stderr, "socket: %d\n", sock_fd);
-  if(sock_fd < 0) {
-    perror("socket");
-    return 1;
-  }
-  
-  // Serverstruktur einrichten 
-  // Datenstruktur auf 0 setzen
-  // Familie: Internetserver
-  // Adresse: maya.rz.hs-fulda.de
-  server_size = sizeof(server_addr);
-  memset((void *) &server_addr, 0, server_size);
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(server_ip);
-  server_addr.sin_port = htons(srv_port);
-  
-  // Nachricht an Server schicken
-  sprintf(buffer, SERVER_MESSAGE, getpid());
-  length = strlen(buffer);
-  fprintf(stderr, "sendto: %d Bytes %s\n", length, buffer);
-  nbytes = sendto(sock_fd, buffer, length, 0, (struct sockaddr *) &server_addr, server_size);  
-  if(nbytes != length) {
-    perror("sendto");
-    return 1;  
-  }
-  
-//   // Antwort vom Server lesen
-//   nbytes = recvfrom(sock_fd, buffer, sizeof(buffer) - 1, 0, NULL, NULL);
-//   if(nbytes < 0) {
-//     perror("recvfrom");
-//     return 1;
-//   }
-//   buffer[nbytes] = '\0';
-//   fprintf(stderr, "recvfrom: %d Bytes %s\n", nbytes, buffer);
+    // Kommandozeile bearbeiten
+    if (argc < 4)
+    {
+        fprintf(stderr, "Aufruf: smbpublish broker topic message\n");
+        return 1;
+    }
+    struct hostent *hostptr;
+    // hostent-Daten lesen
+    if ((hostptr = gethostbyname(argv[1])) == NULL)
+    {
+        fprintf(stderr, "gethostbyname: Host %s nicht gefunden\n\n", argv[1]);
+        return 1;
+    }
 
-  // Socket schliessen und Ende
-  close(sock_fd);
-  return 0;
+    // 1. Parameter -> IP-Adresse des Server
+    char *server_ip = inet_ntoa(*(struct in_addr *)hostptr->h_addr);// Server IP
+    // Socket anlegen und oeffnen
+    // Familie: Internet, Typ: UDP-Socket
+    sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    fprintf(stderr, "socket: %d\n", sock_fd);
+    if (sock_fd < 0)
+    {
+        perror("socket");
+        return 1;
+    }
+
+    // Serverstruktur einrichten
+    // Datenstruktur auf 0 setzen
+    // Familie: Internetserver
+    // Adresse: maya.rz.hs-fulda.de
+    server_size = sizeof(server_addr);
+    memset((void *)&server_addr, 0, server_size);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+    server_addr.sin_port = htons(srv_port);
+
+    // Nachricht an Server schicken
+    sprintf(buffer, SERVER_MESSAGE, getpid());
+    length = strlen(buffer);
+    fprintf(stderr, "sendto: %d Bytes %s\n", length, buffer);
+    nbytes = sendto(sock_fd, buffer, length, 0, (struct sockaddr *)&server_addr, server_size);
+    if (nbytes != length)
+    {
+        perror("sendto");
+        return 1;
+    }
+
+    //   // Antwort vom Server lesen
+    //   nbytes = recvfrom(sock_fd, buffer, sizeof(buffer) - 1, 0, NULL, NULL);
+    //   if(nbytes < 0) {
+    //     perror("recvfrom");
+    //     return 1;
+    //   }
+    //   buffer[nbytes] = '\0';
+    //   fprintf(stderr, "recvfrom: %d Bytes %s\n", nbytes, buffer);
+
+    // Socket schliessen und Ende
+    close(sock_fd);
+    return 0;
 }
--- Klausur/smbsubscribe.c
+++ Klausur/smbsubscribe.c
@@ -15,7 +15,6 @@ smbsubscribe 192.168.1.13 "#"
 if topic is # then werden alle Nachrichten vom Broker an den
 Subscriber weitergeleitet
 
-
 */
 
 #include <stdio.h>
@@ -26,70 +25,80 @@ Subscriber weitergeleitet
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <netdb.h>
 
 // Port
 const int srv_port = 8080;
 
 // Formatstring der Nachricht
-#define SERVER_MESSAGE  "Hi, hier Client %d"
+#define SERVER_MESSAGE "Hi, hier Client %d"
 
 // main
 int main(int argc, char **argv)
 {
-  char *server_ip;                      // Server IP
-  int sock_fd;                          // Socket
-  struct sockaddr_in server_addr;       // Serveradresse
-  socklen_t server_size;                // Adresslaenge
-  char buffer[512];                     // Schreib-/Lesepuffer
-  int nbytes, length;
+    int sock_fd;                    // Socket
+    struct sockaddr_in server_addr; // Serveradresse
+    socklen_t server_size;          // Adresslaenge
+    char buffer[512];               // Schreib-/Lesepuffer
+    int nbytes, length;
 
-  // Kommandozeile bearbeiten
-  if(argc < 2) {
-    fprintf(stderr, "Aufruf: %s Server-IP-Adresse\n", argv[0]);
-    return 1;
-  }
-  // 1. Parameter -> IP-Adresse des Server
-  server_ip = argv[1];
-  
-  // Socket anlegen und oeffnen
-  // Familie: Internet, Typ: UDP-Socket
-  sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
-  fprintf(stderr, "socket: %d\n", sock_fd);
-  if(sock_fd < 0) {
-    perror("socket");
-    return 1;
-  }
-  
-  // Serverstruktur einrichten 
-  // Datenstruktur auf 0 setzen
-  // Familie: Internetserver
-  // Adresse: maya.rz.hs-fulda.de
-  server_size = sizeof(server_addr);
-  memset((void *) &server_addr, 0, server_size);
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = inet_addr(server_ip);
-  server_addr.sin_port = htons(srv_port);
-  
-  // Nachricht an Server schicken
-  sprintf(buffer, SERVER_MESSAGE, getpid());
-  length = strlen(buffer);
-  fprintf(stderr, "sendto: %d Bytes %s\n", length, buffer);
-  nbytes = sendto(sock_fd, buffer, length, 0, (struct sockaddr *) &server_addr, server_size);  
-  if(nbytes != length) {
-    perror("sendto");
-    return 1;  
-  }
-  
-  // Antwort vom Server lesen
-  nbytes = recvfrom(sock_fd, buffer, sizeof(buffer) - 1, 0, NULL, NULL);
-  if(nbytes < 0) {
-    perror("recvfrom");
-    return 1;
-  }
-  buffer[nbytes] = '\0';
-  fprintf(stderr, "recvfrom: %d Bytes %s\n", nbytes, buffer);
+    // Kommandozeile bearbeiten
+    if (argc < 3)
+    {
+        fprintf(stderr, "Aufruf: smbsubscribe broker topic\n");
+        return 1;
+    }
+    struct hostent *hostptr;
+    // hostent-Daten lesen
+    if ((hostptr = gethostbyname(argv[1])) == NULL)
+    {
+        fprintf(stderr, "gethostbyname: Host %s nicht gefunden\n\n", argv[1]);
+        return 1;
+    }
 
-  // Socket schliessen und Ende
-  close(sock_fd);
-  return 0;
+    // 1. Parameter -> IP-Adresse des Server
+    char *server_ip = inet_ntoa(*(struct in_addr *)hostptr->h_addr);// Server IP
+    // Socket anlegen und oeffnen
+    // Familie: Internet, Typ: UDP-Socket
+    sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    fprintf(stderr, "socket: %d\n", sock_fd);
+    if (sock_fd < 0)
+    {
+        perror("socket");
+        return 1;
+    }
+
+    // Serverstruktur einrichten
+    // Datenstruktur auf 0 setzen
+    // Familie: Internetserver
+    // Adresse: maya.rz.hs-fulda.de
+    server_size = sizeof(server_addr);
+    memset((void *)&server_addr, 0, server_size);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+    server_addr.sin_port = htons(srv_port);
+
+    // Nachricht an Server schicken
+    sprintf(buffer, SERVER_MESSAGE, getpid());
+    length = strlen(buffer);
+    fprintf(stderr, "sendto: %d Bytes %s\n", length, buffer);
+    nbytes = sendto(sock_fd, buffer, length, 0, (struct sockaddr *)&server_addr, server_size);
+    if (nbytes != length)
+    {
+        perror("sendto");
+        return 1;
+    }
+
+    //   // Antwort vom Server lesen
+    //   nbytes = recvfrom(sock_fd, buffer, sizeof(buffer) - 1, 0, NULL, NULL);
+    //   if(nbytes < 0) {
+    //     perror("recvfrom");
+    //     return 1;
+    //   }
+    //   buffer[nbytes] = '\0';
+    //   fprintf(stderr, "recvfrom: %d Bytes %s\n", nbytes, buffer);
+
+    // Socket schliessen und Ende
+    close(sock_fd);
+    return 0;
 }
